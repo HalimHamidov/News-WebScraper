6576c2031fc78051ae6d65a63de59bb994640a60
HalimHamidov <halimjonik@gmail.com>
2021-09-29 17:41:53 +0300

    update cron

--- src/crons/entrypoint.sh
+++ src/crons/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setup a cron schedule
-echo "0 * * * * python3 /src/save_todb.py 2>&1
+echo "0 * * * * python3 /src/save_to_db.py 2>&1
 # This extra line makes it a valid cron" >scheduler.txt
 echo "Start cron schedule"
 crontab scheduler.txt
